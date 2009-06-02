6aeaa543910e3bf79a6f46ee168eb093a86ae28e
qa22ahj <qa22ahj@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-02 04:16:56 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@6 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/DavichiGUI.java
+++ newGui/src/DavichiGUI.java
@@ -1,8 +1,6 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
