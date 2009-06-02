5ea38fd328f4d628894231175ff4594a7c71ddfe
qa22ahj <qa22ahj@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-02 21:47:44 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@8 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/Network.java
+++ newGui/src/Network.java
@@ -4,6 +4,6 @@ abstract public class Network
 	protected mWindow m_Taget;//채팅 메세지를 받으면 전달할 대상 
 	protected Network m_net;
 	abstract public void Connet(String ip);//지정된 ip로 접속하는 함수
-	abstract public void SendChatMsg(String msg);//접속된 네트워크에 msg를 날리는 함수
+	abstract public void SendChatMsg(String msg);//접속된 네트워크에 채팅 msg를 날리는 함수
 	abstract public void Close();//모든접속을 끊는 함수. 쓰레드 종료필수
 }
