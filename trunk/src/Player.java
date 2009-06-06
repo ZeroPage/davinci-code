580a16328177b0d3a774d41cac4299a3835d04ca
sibichi <sibichi@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-06 11:55:08 +0000

    조커 예외처리 아직 안된것
    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@47 c97080ab-a217-484f-b99f-215fc1f465bf

--- trunk/src/Player.java
+++ trunk/src/Player.java
@@ -4,24 +4,17 @@ import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 
 public class Player {
-	private int blockNum;//자신이 가진 블록수를 저장
 	private ArrayList<Block> hand;//자신이 가진 블록을 저장하는 배열
 	private boolean play;//플레이여부를 결정
 	
 	Player() {
 		play = true;
 	}
-	public void setBlockNum(int n) {
-		blockNum = n;
-	}
 	public void setHand(Block n) {
 		
 	}
 	public void setPlay(boolean n) {
 		play = n;
-	}
-	public int getBlockNum() {
-		return blockNum;
 	}
 	public ArrayList<Block> getHand() {
 		return hand;
@@ -33,7 +26,13 @@ public class Player {
 	public void getBlock(ArrayList<Block> blocks, int selectedBlock) {
 		hand.add(blocks.get(selectedBlock));
 		blocks.remove(selectedBlock);
-		sortBlock(getHand(), getBlockNum());
+		if(blocks.get(selectedBlock).getNum()==-1)
+		{
+			//원하는 위치에 클릭 삽입
+			//if(삽입위치가 맨 앞)
+		}
+		else if(blocks.get(selectedBlock).getNum()!=-1)
+			sortBlock(getHand(), getBlockNum());
 	}
 	public boolean askBlock(Player tp, Block tb, int selectedNum) {		
 		return tp.checkBlock(tb, selectedNum);
@@ -63,18 +62,18 @@ public class Player {
 		blocks.set(n1,blocks.get(n2));
 		blocks.set(n2,tb);
 	}
-	public void sortBlock(Block[] array, int length) {
-		for(int i=1; i<length; i++) {
-			int j;
-			for(j=i-1; (j>=0) && ((array[i].getNum() > array[j].getNum()) || ((array[i].getNum() == array[j].getNum()) && (array[i].getColor() > array[j].getColor()))); j--)
-				if(array[j].getNum() == -1) {
-					j--;
-					continue;
+	public void sortBlock(ArrayList<Block> blocks, int s, int e) {
+		for(int i = s; i<e;i++)
+		{
+			for(int j=s;j<e;j++){
+				Block tb1 = blocks.get(j);
+				Block tb2 = blocks.get(j+1);
+				if(tb1.getNum()>tb2.getNum())
+				{
+					blocks.set(j, tb2);
+					blocks.set(j+1, tb1);
 				}
-			Block temp = array[i];
-			for(int k=i; (k>=0) && (k>j+1); k--)
-				array[k] = array[k-1];
-			array[j+1] = temp;
+			}
 		}
 	}
 }
