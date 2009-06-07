349203be12aff2fff669ffb3e2ae98b6a0663c60
wisdomtree <wisdomtree@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-07 14:20:56 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@104 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/GameProcess.java
+++ newGui/src/GameProcess.java
@@ -9,7 +9,9 @@ public class GameProcess
 	//gui와 game은 동기화이나 network와의 관계는 적지 않았다. network는 혁수가 적당히 적어주길
 	GameWindow m_GUITaget;
 	Network m_NetTaget;
-	int playOrder;
+	int playOrder; // 자신의 플레이 순서인 모양이다.?
+	//next함수를 쓰면 네트워크에서 브로드캐스팅으로 다음 사람을 찾도록 int형 값을 전송한다.
+	//네트워크에서 int를 받아 이 값과 같아 자신의 차례면 turn을 호출하도록 하겠다.
 	
 	Game GC;
 	
@@ -25,6 +27,9 @@ public class GameProcess
 		//게임을 시작하는 것은 방장만이 할수 있는것 이것에 대한 조치를 취해야 한다.
 		//Network에 isSever함수를 두고 클라에서는 false를 리턴하게 오버로딩을
 		//서버에서는 true를 리턴도록 오버로딩을 하면 된다.(isServer 구현완료.)
+		
+		//게임 시작시 필요한 준비 과정을 여기에서 끝내고
+		//이 함수를 끝내기 전에 네트워크로 데이터를 보내서 모두의 블럭들을 동기화 하자.
 	}
 	public void turn()
 	{
@@ -58,14 +63,15 @@ public class GameProcess
 		//생대방에게 블럭을 물어 봐야 한다.
 		//그러기 위해서는 다른 사람들 패를 하나 골라 선택해야 한다. 따라서 다른 플레이어의 패을 enable 해준다
 	}
-	public void AskBlock(int playerNum, int index)
+	public void AskBlock(int playerNum, int index, int num)
 	{
 		//파라미터가 있으므로 위에 함수와 구분이 된다.몇번 플레이어에 몇번 인덱스라는 것만 알려주면 된다.
+		//어떤 숫자인지 알아야한다.
 		//네트워크에 물어보는 내용을 전송한다.
 	}
-	public void CheekBlock()
+	public void CheekBlock(int index, int num)
 	{
-		//네크워크에서 물음을 받으면 자신의 패를 체크해준다. 그리고 리턴값을 돌려주는것이 아니라 네트워크에 맞는지 틀린지 전송해준다.
+		//네크워크에서 물음을 받으면 자신의 패를 체크해준다.그리고 리턴값을 돌려주는것이 아니라 네트워크에 맞는지 틀린지 전송해준다.
 		//게임의 체크 블록을 부른다. 단 자신의 패가 아니면 건들지 않는다.
 	}
 	public void correct()
