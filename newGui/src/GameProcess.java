eaa8140c81c2373c3e0cc0d54d4474db7ca403bb
qa22ahj <qa22ahj@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-08 18:46:15 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@140 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/GameProcess.java
+++ newGui/src/GameProcess.java
@@ -61,6 +61,11 @@ public class GameProcess
 		//몇번 플레이어의 블록 배열을 리턴하면 된다.
 		return (Block[])GC.getPlayers().get(playerNum).getHand().toArray();//왠만하면 이런 복잡한 형식보다는 Game클래스 에다가 getPlayerBlock함수를 두고 거기서 Block의 배열을 리턴하도록 만들지 그래? 이렇게 짜면 읽기가 힘들어.
 	}
+	public Block[] GetCenterBlocksState()
+	{
+		//가운데 블럭의 상태를 리턴한다.
+		return (Block [])GC.getBlocks().toArray();
+	}
 	public void AskBlock()
 	{
 		//생대방에게 블럭을 물어 봐야 한다.
