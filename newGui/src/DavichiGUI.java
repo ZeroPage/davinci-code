ffe73f8338e0ec98d69df76242a739720b20ba57
qa22ahj <qa22ahj@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-08 14:57:47 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@118 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/DavichiGUI.java
+++ newGui/src/DavichiGUI.java
@@ -1,18 +1,14 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Insets;
-import java.awt.Label;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.net.URL;
+
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -218,8 +214,7 @@ public class DavichiGUI extends JFrame
 					//this.setSize(BG.getIconWidth(), BG.getIconHeight());
 					super.paint(g);
 				}
-			}
-			);
+			});
 			TagetChat = chat; 
 			//TagetNetwork = Taget;
 			
