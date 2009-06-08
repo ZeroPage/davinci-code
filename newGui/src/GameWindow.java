30b82bb261f9d338843e7f9a6f2a4fa39a1b02af
qa22ahj <qa22ahj@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-08 20:50:03 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@154 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/GameWindow.java
+++ newGui/src/GameWindow.java
@@ -1,8 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
