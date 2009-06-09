80ee1e7e2dcd81fb2cd058c421f6a6a42a65b970
wisdomtree <wisdomtree@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-09 06:12:28 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@186 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/GameProcess.java
+++ newGui/src/GameProcess.java
@@ -121,7 +121,7 @@ public class GameProcess
 	{
 		//다음 플레이어에게 턴을 넘겨준다. 게임 윈도우의 모든 입력은 블록 처리 되어 있으므로 자동으로 대기상태가 된다. 
 
-		m_NetTaget.SendOb(new DataHeader("pass", Integer.valueOf((playOrder+1)%GC.getPlayers().size())));
+		m_NetTaget.SendOb(new DataHeader("pass", ((Integer.valueOf((playOrder+1))%(GC.getPlayers().size())))));
 	}
 	public void End()
 	{
--- newGui/src/Server.java
+++ newGui/src/Server.java
@@ -125,7 +125,6 @@ public class Server extends Network
 						try
 						{
 							clients[i].setClientData(ObServer.accept());
-							clients[i].SendOb(new DataHeader("접속순서", Integer.valueOf(i+1)));
 						} catch (IOException e)
 						{
 							// TODO Auto-generated catch block
