a259c2efd2a0d222e741f257d4b23c18701a9bec
qa22ahj <qa22ahj@c97080ab-a217-484f-b99f-215fc1f465bf>
2009-06-09 13:11:25 +0000

    git-svn-id: http://nforge.zeropage.org/svn/davinchicode@208 c97080ab-a217-484f-b99f-215fc1f465bf

--- newGui/src/GameWindow.java
+++ newGui/src/GameWindow.java
@@ -107,7 +107,7 @@ class GameWindow
 			PlayerNumToWindowNum = new int[Process.getPlayerNum()];
 			for(int i = 0; i < PlayerNumToWindowNum.length; i++)
 			{
-				PlayerNumToWindowNum[i] = (Process.playOrder + i )%4;
+				PlayerNumToWindowNum[i] = (Process.playOrder + i )%PlayerNumToWindowNum.length;
 			}
 			Process.turn();
 		}
@@ -122,7 +122,7 @@ class GameWindow
 		PlayerNumToWindowNum = new int[PlayerNum];
 		for(int i = 0; i < PlayerNumToWindowNum.length; i++)
 		{
-			PlayerNumToWindowNum[i] = (Process.playOrder + i )%4;
+			PlayerNumToWindowNum[i] = (Process.playOrder + i )%PlayerNum;
 		}
 	}
 	class PlayerWindow implements ActionListener
